152ad9d14a312066603424bb3640dea31a24dabe
Seyfettin <334321@ogr.ktu.edu.tr>
2022-04-14 02:25:31 +0300

    Set up providers as DI

---
+++ lib/providers.dart
@@ -0,0 +1,26 @@
+import 'package:lolwiki/data/data_sources/data_dragon.dart';
+import 'package:lolwiki/data/repositories/champion_repository.dart';
+import 'package:lolwiki/data/repositories/version_repository.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+import 'presentation/notifiers/lang_notifier.dart';
+import 'presentation/notifiers/theme_notifier.dart';
+
+List<SingleChildWidget> providers = [
+  ...independentServices,
+  ...dependentServices,
+];
+
+List<SingleChildWidget> independentServices = [
+  ChangeNotifierProvider<ThemeNotifier>(create: (_) => ThemeNotifier()),
+  ChangeNotifierProvider<LangNotifier>(create: (_) => LangNotifier()),
+  Provider.value(value: DataDragonAPI()),
+];
+
+List<SingleChildWidget> dependentServices = [
+  ProxyProvider<DataDragonAPI, VersionRepository>(
+      update: (_, dataDragonAPI, __) => VersionRepository(dataDragonAPI)),
+  ProxyProvider<DataDragonAPI, ChampionRepository>(
+      update: (_, dataDragonAPI, __) => ChampionRepository(dataDragonAPI)),
+];
