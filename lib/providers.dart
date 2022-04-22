9b2bc42676a52435536ddeaa29c57c05dc5bad41
Seyfettin <334321@ogr.ktu.edu.tr>
2022-04-23 02:02:43 +0300

    Inject dependencies to the use cases

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,6 +1,9 @@
 import 'package:leaguechamps/data/data_sources/data_dragon.dart';
+import 'package:leaguechamps/data/data_sources/hive_service.dart';
 import 'package:leaguechamps/data/repositories/champion_repository.dart';
 import 'package:leaguechamps/data/repositories/version_repository.dart';
+import 'package:leaguechamps/domain/use_cases/champions_use_case.dart';
+import 'package:leaguechamps/domain/use_cases/version_use_case.dart';
 import 'package:leaguechamps/presentation/notifiers/connectivity_notifier.dart';
 import 'package:leaguechamps/presentation/notifiers/version_notifier.dart';
 import 'package:provider/provider.dart';
@@ -14,16 +17,33 @@ List<SingleChildWidget> providers = [
 ];
 
 List<SingleChildWidget> independentServices = [
-  ChangeNotifierProvider<ThemeNotifier>(create: (_) => ThemeNotifier()),
-  ChangeNotifierProvider<VersionNotifier>(create: (_) => VersionNotifier()),
+  ChangeNotifierProvider<ThemeNotifier>(
+    create: (_) => ThemeNotifier(),
+  ),
+  ChangeNotifierProvider<VersionNotifier>(
+    create: (_) => VersionNotifier(),
+  ),
   ChangeNotifierProvider<ConnectivityNotifier>(
-      create: (_) => ConnectivityNotifier()),
-  Provider.value(value: DataDragonAPI()),
+    create: (_) => ConnectivityNotifier(),
+  ),
+  Provider.value(
+    value: DataDragonAPI(),
+  ),
 ];
 
 List<SingleChildWidget> dependentServices = [
   ProxyProvider<DataDragonAPI, VersionRepository>(
-      update: (_, dataDragonAPI, __) => VersionRepository(dataDragonAPI)),
+    update: (_, dataDragonAPI, __) => VersionRepository(dataDragonAPI),
+  ),
   ProxyProvider<DataDragonAPI, ChampionRepository>(
-      update: (_, dataDragonAPI, __) => ChampionRepository(dataDragonAPI)),
+    update: (_, dataDragonAPI, __) => ChampionRepository(dataDragonAPI),
+  ),
+  ProxyProvider2<ChampionRepository, HiveService, ChampionUseCase>(
+    update: (_, championRepository, hiveService, __) =>
+        ChampionUseCase(championRepository, hiveService),
+  ),
+  ProxyProvider2<ChampionRepository, HiveService, VersionUseCase>(
+    update: (_, championRepository, hiveService, __) =>
+        VersionUseCase(championRepository, hiveService),
+  ),
 ];
