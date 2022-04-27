77fdf686b4cd8137d4832d6f137b5ce59c7ba2dc
seyfettiny <syfttny@gmail.com>
2022-04-27 17:53:01 +0300

    Implement UserCases for version and champion repositories

--- lib/domain/use_cases/champions_use_case.dart
+++
@@ -1,7 +0,0 @@
-class ChampionUseCase {
-  final _championRepository;
-  final _hiveService;
-  const ChampionUseCase(this._championRepository,this._hiveService);
-
-  //TODO: decide whether retrieve data from cache or from network
-}
