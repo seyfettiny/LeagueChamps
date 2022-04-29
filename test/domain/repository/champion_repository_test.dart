37e71a32a103bd481279293a964347cb2a6c1edd
seyfettiny <334321@ogr.ktu.edu.tr>
2022-04-29 14:15:16 +0300

    Remove generated mockito files from source control

---
+++ test/domain/repository/champion_repository_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:leaguechamps/data/repositories/champion_repository.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([ChampionRepository])
+void main() {
+}
