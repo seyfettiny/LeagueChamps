5fb53b297294dff8ed0bbd3dd946271d1c6740e2
seyfettiny <334321@ogr.ktu.edu.tr>
2022-04-30 14:10:28 +0300

    Include ConnectivityService with GenerateMocks

---
+++ test/app/utils/connectivity_service_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:leaguechamps/app/utils/connectivity_service.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([ConnectivityService])
+void main() {
+  testWidgets('connectivity service ...', (tester) async {
+    // TODO: Implement test
+  });
+}
